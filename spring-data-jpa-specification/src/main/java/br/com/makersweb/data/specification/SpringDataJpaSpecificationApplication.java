3f4c8b5f59f3880f714d645ecc4f29577d152f53
Anderson O. Aristides <marrecogevans@gmail.com>
2018-08-05 18:22:22 -0300

    Commit Inicial

---
+++ spring-data-jpa-specification/src/main/java/br/com/makersweb/data/specification/SpringDataJpaSpecificationApplication.java
@@ -0,0 +1,12 @@
+package br.com.makersweb.data.specification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaSpecificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaSpecificationApplication.class, args);
+    }
+}
